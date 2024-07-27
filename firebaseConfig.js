88ac41ad2aeed8d4af09bb523a683139b19529da
ianmarcel <ianmarcelcampos.com>
2024-07-27 16:07:53 -0300

    criando o config do firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9KXoQob8sPlGziT8vJNdr72DJwm0xRq9g",
+  authDomain: "central-grafica-a134f.firebaseapp.com",
+  projectId: "central-grafica-a134f",
+  storageBucket: "central-grafica-a134f.appspot.com",
+  messagingSenderId: "961137480814",
+  appId: "1:961137480814:web:88c35d53a2c9d1fefd0e1f",
+  measurementId: "G-WBWTZNR89D"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
